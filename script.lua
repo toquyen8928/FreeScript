cc7d373c0da525018cbfbe872e8eab038107064f
toquyen8928 <84212726+toquyen8928@users.noreply.github.com>
2022-10-27 22:57:02 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,5 +2,8 @@ repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/BF")()
-elseif PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
+elseif PlaceId == 3956818381 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/toquyen8928/FreeScript/main/stuff/nl-3956818381.lua")()
+elseif PlaceId == 286090429 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt")()
 end
