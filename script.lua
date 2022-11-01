7a21dcee61887c6bbccd4f064c1841be8f5fd1e3
toquyen8928 <84212726+toquyen8928@users.noreply.github.com>
2022-11-01 20:52:35 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,7 +3,7 @@ local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
 elseif PlaceId == 3956818381 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/toquyen8928/FreeScript/main/stuff/nl-3956818381.lua")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/toquyen8928/FreeScript/main/Quyen%20Hub/nl-3956818381.lua")()
 elseif PlaceId == 286090429 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/BoltsHubV5"))()
 end
