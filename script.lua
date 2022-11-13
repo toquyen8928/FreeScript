d22824b9478d52726a7a9699ba280c7725bce4df
Quyền Kuns <84212726+toquyen8928@users.noreply.github.com>
2022-11-13 19:14:17 +0700

    Remove Error Scripts

--- script.lua
+++ script.lua
@@ -1,11 +1,11 @@
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")() -- Blox Fruits
 elseif PlaceId == 3956818381 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/toquyen8928/FreeScript/main/Quyen%20Hub/nl-3956818381.lua")()
+    loadstring(game:HttpGet"")() -- Ninja Lengends
 elseif PlaceId == 286090429 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/BoltsHubV5"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/BoltsHubV5"))() -- Arsena;
 elseif PlaceId == 9872472334 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/toquyen8928/FreeScript/main/Quyen%20Hub/ev-9872472334.lua"))()
+    loadstring(game:HttpGet(""))() -- Evade
 end
