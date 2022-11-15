7d57e45d92544e046047da0a944007dc8dc09b9a
Quyền Kuns <84212726+toquyen8928@users.noreply.github.com>
2022-11-15 20:24:43 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -5,7 +5,7 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
 elseif PlaceId == 3956818381 then
     loadstring(game:HttpGet"")() -- Ninja Lengends
 elseif PlaceId == 286090429 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/BoltsHubV5"))() -- Arsena;
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/BoltsHubV5"))() -- Arsenal
 elseif PlaceId == 6600532334 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/toquyen8928/FreeScript/main/TCQ%20Hub/QuyenHub.lua"))() -- Test
 end
