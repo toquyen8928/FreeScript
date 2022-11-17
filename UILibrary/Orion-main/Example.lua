1f57526ac726101ea3209b12e849c462b06a7131
Quyền Kuns <84212726+toquyen8928@users.noreply.github.com>
2022-11-17 16:25:37 +0700

    Add files via upload

---
+++ UILibrary/Orion-main/Example.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 11346342371 then
+	local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/shlexware/Orion/main/source")))()
+	local Window = OrionLib:MakeWindow({Name = "Quyen Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "QuyenHub"})
+	
+	
+	
+end
+OrionLib:Init()
