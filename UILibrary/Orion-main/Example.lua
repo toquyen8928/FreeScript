c485a6437358900cf90117dda5fc2af3bb1a6193
Quyền Kuns <84212726+toquyen8928@users.noreply.github.com>
2022-11-17 16:29:02 +0700

    Delete UILibrary/Orion-main directory

--- UILibrary/Orion-main/Example.lua
+++
@@ -1,8 +0,0 @@
-if game.PlaceId == 11346342371 then
-	local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/shlexware/Orion/main/source")))()
-	local Window = OrionLib:MakeWindow({Name = "Quyen Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "QuyenHub"})
-	
-	
-	
-end
-OrionLib:Init()
